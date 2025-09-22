e0e23479d32aa1c9793faeb377e2ed1efef9b44d
Kartik Patel <kartikpat25@gmail.com>
2025-09-21 20:32:08 -0400

    feat: add colorful text for Navbar name and links

--- app/font.js
+++ app/font.js
@@ -1,4 +1,5 @@
-import { Lato, Pacifico } from 'next/font/google';
+import { Cookie, Lato, Pacifico } from 'next/font/google';
 
 export const lato = Lato({ subsets: ['latin'], weight: ['400','700'] });
 export const pacifico = Pacifico({ subsets: ['latin'], weight: '400' });
+export const cookie = Cookie({subsets: ['latin'], weight: '400' })
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,34 @@
-import { pacifico } from '../app/font';
+import { cookie } from '../app/font';
 
 export default function NavBar() {
     return (
-        <div className="w-full h-[8vh] flex flex-row justify-between items-center font-sans">
-            <h1 className="p-4">Kartik Patel</h1>
-            <div className={`${pacifico.className} + flex gap-8 p-8 font-nav`}>
-                <a href="#hero" className="shadow-md">Home</a>
-                <a href="#projects">Projects</a>
-                <a href="#projects">Stats</a>
-                <a href="#contact">Contact</a>
+        <div className="w-full h-[8vh] flex flex-row justify-between items-center font-sans p-10">
+            <h1 className={`${cookie.className} p-8 text-purple-600 text-xl sm:text-2xl md:text-3xl lg:text-4xl`}>Kartik Patel</h1>
+            <div className={`${cookie.className} text-xl sm:text-2xl md:text-3xl lg:text-4xl flex gap-8 p-8 text-blue-500`}>
+                <a
+                className={`${cookie.className} text-blue-500 hover:text-pink-500 underline-center relative`}
+                href="#hero"
+                >
+                Home
+                </a>
+                <a
+                className={`${cookie.className} text-green-500 hover:text-purple-500 underline-center relative`}
+                href="#projects"
+                >
+                Projects
+                </a>
+                <a
+                className={`${cookie.className} text-yellow-600 hover:text-indigo-400 underline-center relative`}
+                href="#stats"
+                >
+                Stats
+                </a>
+                <a
+                className={`${cookie.className} text-pink-500 hover:text-blue-500 underline-center relative`}
+                href="#contact"
+                >
+                Contact
+                </a>
             </div>
         </div>
     );
