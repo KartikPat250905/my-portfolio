af351588fc3b4f4bc7900141a07cc0aeb78bbe1b
Kartik Patel <kartikpat25@gmail.com>
2025-10-28 21:59:11 -0400

    chore: reordered data according to timeline

--- data/hackathonData.js
+++ data/hackathonData.js
@@ -1,11 +1,11 @@
 export const sampleHackathons = [
     {
-        name: "Junction Hackathon 2024",
-        description: "Developed a model that predicts input amounts based on water loss at each production stage to minimize end-product standard deviation. Achieved 97% accuracy with the provided dataset. ",
-        technologies: [ "Python", "TensorFlow", "Pandas", "NumPy", "Scikit-learn", "Matplotlib", "Figma"],
-        position: "Top 5 Finalist",
+        name: "Supercell AI Hackathon",
+        description: "Built a prototype for the “Context-Aware Characters” challenge at the Supercell AI Hackathon — a game where the player controls a cat and the AI reacts dynamically with context-aware dialogue and actions. Recognized among 50+ global projects.",
+        technologies: ["Unity", "C#", "Excalidraw"],
+        position: "3rd Place",
         isWinner: true,
-        category: "AI/ML"
+        category: "Game Development / AI"
     },
     {
         name: "Student Network Challenge Hackathon",
@@ -16,11 +16,11 @@ export const sampleHackathons = [
         category: "Idea"
     },
     {
-        name: "Supercell AI Hackathon",
-        description: "Built a prototype for the “Context-Aware Characters” challenge at the Supercell AI Hackathon — a game where the player controls a cat and the AI reacts dynamically with context-aware dialogue and actions. Recognized among 50+ global projects.",
-        technologies: ["Unity", "C#", "Excalidraw"],
-        position: "3rd Place",
+        name: "Junction Hackathon 2024",
+        description: "Developed a model that predicts input amounts based on water loss at each production stage to minimize end-product standard deviation. Achieved 97% accuracy with the provided dataset. ",
+        technologies: [ "Python", "TensorFlow", "Pandas", "NumPy", "Scikit-learn", "Matplotlib", "Figma"],
+        position: "Top 5 Finalist",
         isWinner: true,
-        category: "Game Development / AI"
-    },
+        category: "AI/ML"
+    }
 ];
