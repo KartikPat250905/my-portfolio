e881d4b7b6ee0d0485ff35effe63135ffde37b99
Kartik Patel <kartikpat25@gmail.com>
2025-10-29 19:42:34 -0400

    chore: added color toggle styling for light and dark mode + changed responsiveness settings for smaller devices

--- components/ProjectSection.js
+++ components/ProjectSection.js
@@ -3,7 +3,7 @@ import { ProjectData } from "../data/ProjectData";
 
 export default function ProjectsSection() {
   return (
-    <div className="flex flex-row flex-wrap px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 2xl:px-52" id="projects">
+    <div className="flex flex-row flex-wrap px-2 sm:px-8 md:px-16 lg:px-24 xl:px-32 2xl:px-52" id="projects">
       {ProjectData.map((project, index) => (
         <ProjectCard key={index} {...project} />
       ))}
